9cc92e7fb7e3a005dece3b33f822666f3c7a1523
oospina <oscarospina@worldoffice.com.co>
2025-03-11 19:16:27 -0500

    se le agrega swagger a la aplicacion y se documenta

--- src/main/java/co/com/oospina/userhexagonalservice/infrastructure/controllers/usercomplete/UserCompleteController.java
+++ src/main/java/co/com/oospina/userhexagonalservice/infrastructure/controllers/usercomplete/UserCompleteController.java
@@ -1,27 +1,3 @@
-//package co.com.oospina.userhexagonalservice.infrastructure.controllers.usercomplete;
-//
-//import co.com.oospina.userhexagonalservice.application.services.usercomplete.IUsersComplete;
-//import lombok.AllArgsConstructor;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//
-//@RequestMapping("/api/v1")
-//@AllArgsConstructor
-//public class UserCompleteController {
-//
-//    private final IUsersComplete iUsersById;
-//
-//    @GetMapping("/users/{id}")
-//    public ResponseEntity<?> getUserByIdAndPokemonId(@PathVariable Long id) {
-//        return ResponseEntity.ok(iUsersById.getUserComplete(id));
-//    }
-//}
-
 package co.com.oospina.userhexagonalservice.infrastructure.controllers.usercomplete;
 
 import co.com.oospina.userhexagonalservice.application.services.usercomplete.IUsersComplete;
